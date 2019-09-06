27b0d80ea3e9be15d946e768e2748d71ca716aa9
Augusto Xavier <augustocbx@gmail.com>
2019-09-06 00:19:36 -0300

    Input for search and layout Add Podfile

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:buscador_gifs/ui/home_page.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomePage(),
-));
-
+      home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)))),
+    ));
